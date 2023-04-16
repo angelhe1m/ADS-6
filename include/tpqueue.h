9869de6236f7ea80b3b6df3f7d96074a5dfc375d
Daniel Khashev (Angelhe1m) <116936326+angelhe1m@users.noreply.github.com>
2023-04-16 21:12:23 +0300

    1 demo

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -4,7 +4,60 @@
 
 template<typename T, int size>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на кольцевом буфере
+ private:
+  T* arr;
+  int startQueue;
+  int first, last, k;
+ public:
+  TPQueue():startQueue(size), first(0), last(0), k(0) {
+   arr = new T[startQueue + 1];
+  }
+  
+  ~TPQueue() {
+    delete[] arr;
+  }
+  bool isFull() const {
+    return k == startQueue;
+  }
+  bool isEmpty() const {
+    return k == 0;
+  }
+  char get() {
+    assert(k > 0);
+    return arr[first].ch;
+  }
+  void push(const T& value) {
+    assert(k < startQueue);
+    if (k == 0) {
+      arr[last++] = value;
+      k++;
+    } else {
+      int i = last - 1;
+      bool flag = 0;
+      while (i >= first && value.prior > arr[i].prior) {
+        flag = 1;
+        arr[i + 1] = arr[i];
+        arr[i] = value;
+        i--;
+      }
+      if (flag == 0) {
+        arr[last] = value;
+      }
+      last++;
+      k++;
+    }
+    if (last > startQueue) {
+      last -= startQueue;
+    }
+  }
+  const T& pop() {
+    assert(k > 0);
+    k--;
+    if (first > startQueue) {
+      first -= startQueue;
+    }
+    return arr[first++];
+  }
 };
 
 struct SYM {
